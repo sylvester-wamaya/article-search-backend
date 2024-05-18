5c36624517d119859d03323f4c571c4eb970d1a6
sylvester wamaya <sylvesterwamaya@gmail.com>
2024-05-18 04:49:11 +0300

    Added routes to statistics

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :articles
-  resources :searches
+  resources :searches, only: [:index, :create]
+  get "searches/search_stats" => "searches#search_stats"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
