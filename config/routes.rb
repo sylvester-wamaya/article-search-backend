7028d447778a005a1397ca4c6a6ef1a320528285
sylvester wamaya <sylvesterwamaya@gmail.com>
2024-05-21 12:07:25 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users
   resources :articles
   resources :searches
-  get "searches/stats" => "searches#search_stats"
+  get "searches_stats" => "searches#search_stats"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
