5dd7587e5760b57bdea2f604a3b512fb16c47c4d
sylvester wamaya <sylvesterwamaya@gmail.com>
2024-05-21 05:17:49 +0300

    Add header responses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+    protect_from_forgery
+
+    before_action :set_cors_headers
+  
+    private
+  
+    def set_cors_headers
+      headers['Access-Control-Allow-Origin'] = '*'  # Replace with your desired origin(s)
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'  # Adjust as needed
+      headers['Access-Control-Allow-Headers'] = '*'  # Customize allowed headers
+      headers['Access-Control-Max-Age'] = '1728000'  # Optional: Set preflight cache duration
+    end
 end
