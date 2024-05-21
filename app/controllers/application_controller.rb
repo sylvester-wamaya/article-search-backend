80917871d70a2c16ee4c65dc47a493d0d07fb727
sylvester wamaya <sylvesterwamaya@gmail.com>
2024-05-21 05:44:08 +0300

    Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < ActionController::API
     private
   
     def set_cors_headers
-      headers['Access-Control-Allow-Origin'] = '*'  # Replace with your desired origin(s)
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'  # Adjust as needed
-      headers['Access-Control-Allow-Headers'] = '*'  # Customize allowed headers
-      headers['Access-Control-Max-Age'] = '1728000'  # Optional: Set preflight cache duration
+      response.headers['Access-Control-Allow-Origin'] = '*'  # Replace with your desired origin(s)
+      response.headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'  # Adjust as needed
+      response.headers['Access-Control-Allow-Headers'] = '*'  # Customize allowed headers
+      response.headers['Access-Control-Max-Age'] = '1728000'  # Optional: Set preflight cache duration
     end
 end
