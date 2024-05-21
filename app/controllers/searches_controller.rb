7c9fba5f256a9f5fe9aaa126ea41e1266db398ca
sylvester wamaya <sylvesterwamaya@gmail.com>
2024-05-21 03:45:23 +0300

    Update post frequency

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,11 +9,13 @@ class SearchesController < ApplicationController
     # GET /search statistics
     def search_stats
       @user = User.find_by(ip_address: request.remote_ip) 
+     
+      
       @search_count = @user.searches.distinct.pluck(:search, :ip).map { |search| {search: search, count: Search.where(search: search).count} }
       
       # Sort the search summary by count in descending order
       @search_summary = @search_count.sort_by { |search| search[:count] }.reverse
-      render json: @search_summary 
+      render json: @search_summary
     
     end
 
@@ -23,20 +25,39 @@ class SearchesController < ApplicationController
       @user = User.find_by(ip_address: @ip_address) || User.create!(username: @ip_address , ip_address: @ip_address, password: "password")
       @search = params[:searchValue]
 
-      # Check if the search value already exists in the database
-      @existing_search = @user.searches.where("search LIKE ?", "%#{@search}%").first
+      
+      @existing_searches = @user.searches || []
 
-      if @existing_search.present? && Search.find_by(search: @search)
+      if check_incomplete_searches(@existing_searches, @search).empty?
         Search.create!(search: @search, ip: @ip_address, user_id: @user.id)
       end
-      if @existing_search.nil?
-        # Create a new Search record only if it doesn't exist
-        Search.create!(search: @search, ip: @ip_address, user_id: @user.id)
-      end  
+    
 
       head :ok
     end
+
+    private
+
+    def check_incomplete_searches(arr, search)
+      @filtered_arr = []
+
+    
+      arr.each do |obj|
+       
+        if obj[:search].start_with?(search) && obj[:search].length > search.length
+          @filtered_arr << obj
+        end
+        if search.start_with?(obj[:search]) && search.length > obj[:search].length
+          obj.destroy
+      end
+    end
+    
+      @filtered_arr
+    end
+
   end
+    
+  
   
 
 
