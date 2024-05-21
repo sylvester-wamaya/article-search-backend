ff661709a1e0242d1766ea5c08d6fd1a75a3cc53
sylvester wamaya <sylvesterwamaya@gmail.com>
2024-05-21 03:45:47 +0300

    Add destroy dependency

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,3 +1,5 @@
 class Search < ApplicationRecord
     belongs_to :user
+    
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ApplicationRecord
-    has_many :searches
+    has_many :searches, :dependent => :destroy
 end
